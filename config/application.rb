6dc775541cc09974ceda851ddc55ab7e2ef86e35
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-27 14:34:06 -0400

    add inline comment on purpose of dotenv::railtie.lad code snippet

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
+Dotenv::Railtie.load #quicker initialization of dotenv gem, NOT NECESSARY BUT PRECAUTION
 
 module AlphaBlog
   class Application < Rails::Application
